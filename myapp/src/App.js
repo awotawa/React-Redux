d5357aa5ef8a6cf053a737694c4c9af715706d9a
Alexandre Wotawa <awotawa@gmail.com>
2021-04-06 14:01:24 +0200

    created a nested Ninjas component

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,21 +1,16 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import Ninjas from './Ninjas';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>My first React app!</h1>
+        <p>Welcome :)</p>
+        <Ninjas />
+      </div>
+    );
+  }
 }
 
 export default App;
