f6458111da1562d4309965c8980688d994672f06
Alexandre Wotawa <awotawa@gmail.com>
2021-04-06 19:22:48 +0200

    implemented the logic in the template itself

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -14,18 +14,28 @@ const Ninjas = ({ninjas}) => { // direct destructuring
   //     return null;
   //   }
   // })
-  const ninjaList = ninjas.map(ninja => {
-    return ninja.age > 20 ? (
-      <div className="ninja" key={ninja.id}>
-        <div>Name: { ninja.name }</div>
-        <div>Age: { ninja.age }</div>
-        <div>Belt: { ninja.belt }</div>
-      </div>
-    ) : null;
-  })
+  // const ninjaList = ninjas.map(ninja => {
+  //   return ninja.age > 20 ? (
+  //     <div className="ninja" key={ninja.id}>
+  //       <div>Name: { ninja.name }</div>
+  //       <div>Age: { ninja.age }</div>
+  //       <div>Belt: { ninja.belt }</div>
+  //     </div>
+  //   ) : null;
+  // })
   return(
     <div className='ninja-list'>
-      {ninjaList}
+      {
+        ninjas.map(ninja => {
+          return ninja.age > 20 ? (
+            <div className="ninja" key={ninja.id}>
+              <div>Name: {ninja.name}</div>
+              <div>Age: {ninja.age}</div>
+              <div>Belt: {ninja.belt}</div>
+            </div>
+          ) : null;
+        })
+      }
     </div>
   );
 }
