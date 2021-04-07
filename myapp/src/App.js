ae74be2dc00afcd37aa40b379a3860a8695e2ecd
Alexandre Wotawa <awotawa@gmail.com>
2021-04-07 12:40:38 +0200

    creating a function as props to update the main state with the form info

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -10,13 +10,20 @@ class App extends Component {
       { name: 'Crystal', age: 20, belt: 'pink', id: 3 }
     ]
   }
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja]; // creating a "carbon copy" of the state.ninjas + the new ninja
+    this.setState({
+      ninjas: ninjas
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1>My first React app!</h1>
         <p>Welcome :)</p>
         <Ninjas ninjas={this.state.ninjas} />
-        <AddNinja />
+        <AddNinja addNinja={this.addNinja}/>
       </div>
     );
   }
