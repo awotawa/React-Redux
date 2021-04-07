4c7f805929eb713f3fd4f4c6a1396e890aec0adc
Alexandre Wotawa <awotawa@gmail.com>
2021-04-07 16:22:27 +0200

    implement a way to delete ninja data

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Ninjas = ({ninjas}) => { // direct destructuring
+const Ninjas = ({ninjas, deleteNinja}) => { // direct destructuring
   // const ninjaList = ninjas.map(ninja => {
   //   if (ninja.age > 20){
   //     return(
@@ -14,28 +14,19 @@ const Ninjas = ({ninjas}) => { // direct destructuring
   //     return null;
   //   }
   // })
-  // const ninjaList = ninjas.map(ninja => {
-  //   return ninja.age > 20 ? (
-  //     <div className="ninja" key={ninja.id}>
-  //       <div>Name: { ninja.name }</div>
-  //       <div>Age: { ninja.age }</div>
-  //       <div>Belt: { ninja.belt }</div>
-  //     </div>
-  //   ) : null;
-  // })
+  const ninjaList = ninjas.map(ninja => {
+    return ninja.age > 20 ? (
+      <div className="ninja" key={ninja.id}>
+        <div>Name: { ninja.name }</div>
+        <div>Age: { ninja.age }</div>
+        <div>Belt: { ninja.belt }</div>
+        <button onClick={() => {deleteNinja(ninja.id)}}>Delete ninja</button>
+      </div>
+    ) : null;
+  })
   return(
     <div className='ninja-list'>
-      {
-        ninjas.map(ninja => {
-          return ninja.age > 20 ? (
-            <div className="ninja" key={ninja.id}>
-              <div>Name: {ninja.name}</div>
-              <div>Age: {ninja.age}</div>
-              <div>Belt: {ninja.belt}</div>
-            </div>
-          ) : null;
-        })
-      }
+      {ninjaList}
     </div>
   );
 }
